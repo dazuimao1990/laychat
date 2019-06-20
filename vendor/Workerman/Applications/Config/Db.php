9b3409cb48b6ed20bda60611904e6602e28acb46
guox <guox@goodrain.com>
2019-06-20 13:12:03 +0800

    Update Db.php

--- vendor/Workerman/Applications/Config/Db.php
+++ vendor/Workerman/Applications/Config/Db.php
@@ -16,9 +16,9 @@ class Db
     public static $db1 = array(
         'host'    => '127.0.0.1',
         'port'    => 3306,
-        'user'    => 'root',
-        'password' => 'root',
-        'dbname'  => 'snake',
+        'user'    => 'admin',
+        'password' => 'laychat',
+        'dbname'  => 'laychat',
         'charset'    => 'utf8'
     );
 }
